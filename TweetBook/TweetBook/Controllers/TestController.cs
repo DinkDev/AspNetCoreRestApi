7798f27459a2631bde6ac8582ddcee5fb956d910
Dale Thompson <dale.thompson@vastec.com>
2020-10-25 20:43:01 -0400

    Episode 1 complete.
    Setup and Swagger configuration - https://youtu.be/sdlt3-ptt9g

---
+++ TweetBook/TweetBook/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿namespace TweetBook.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TestController : Controller
+    {
+        [HttpGet(@"api/user")]
+        public IActionResult Get()
+        {
+            return Ok(new {Name = "Dink"});
+        }
+    }
+}
