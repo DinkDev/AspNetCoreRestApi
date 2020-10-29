02feea80f5705d1d7229b6910efcb5bf6688c520
Dale Thompson <dale.thompson@vastec.com>
2020-10-28 20:54:40 -0400

    Episode 4 complete.

---
+++ TweetBook/TweetBook/Contracts/V1/Requests/CreatePostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TweetBook.Contracts.V1.Requests
+{
+    using System.Runtime.InteropServices.ComTypes;
+
+    public class CreatePostRequest
+    {
+        public string Id { get; set; }
+    }
+}
--- TweetBook/TweetBook/Controllers/V1/PostsController.cs
+++ TweetBook/TweetBook/Controllers/V1/PostsController.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using Contracts.V1;
+    using Contracts.V1.Requests;
+    using Contracts.V1.Responses;
     using Microsoft.AspNetCore.Mvc;
 
     using Domain;
@@ -24,6 +26,27 @@
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        // TODO: should have a contract
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post {Id = postRequest.Id};
+
+            if (string.IsNullOrEmpty(post.Id))
+            {
+                post.Id = Guid.NewGuid().ToString();
+            }
+
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = $"{baseUrl}/{ApiRoutes.Posts.Get.Replace("{postId}", post.Id)}";
+
+            var postResponse = new PostResponse {Id = post.Id};
+
+            return Created(locationUri, postResponse);
         }
     }
 }
